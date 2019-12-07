cc6e039b79ef8f86abd0d0221ad9ab3f754a5a20
YunusNS <yunusw718@gmail.com>
2019-12-07 23:52:41 +0700

    change anime & season anime model properties to optional

--- lib/models/season/season_anime.dart
+++ lib/models/season/season_anime.dart
@@ -18,14 +18,14 @@ class SeasonAnime {
     String toRawJson() => json.encode(toJson());
 
     factory SeasonAnime.fromJson(Map<String, dynamic> json) => SeasonAnime(
-        seasonName: json["season_name"],
-        seasonYear: json["season_year"],
-        anime: List<Anime>.from(json["anime"].map((x) => Anime.fromJson(x))),
+        seasonName: json["season_name"] == null ? null : json["season_name"],
+        seasonYear: json["season_year"] == null ? null : json["season_year"],
+        anime: json["anime"] == null ? null : List<Anime>.from(json["anime"].map((x) => Anime.fromJson(x))),
     );
 
     Map<String, dynamic> toJson() => {
-        "season_name": seasonName,
-        "season_year": seasonYear,
-        "anime": List<dynamic>.from(anime.map((x) => x.toJson())),
+        "season_name": seasonName == null ? null : seasonName,
+        "season_year": seasonYear == null ? null : seasonYear,
+        "anime": anime == null ? null : List<dynamic>.from(anime.map((x) => x.toJson())),
     };
 }

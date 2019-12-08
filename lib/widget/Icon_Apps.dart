88f19fb8f1baddbd48dc7e52fa335013c2e11806
ramadhanfn <ramadhanfirda27@gmail.com>
2019-12-08 15:46:00 +0700

    create class IconApp

---
+++ lib/widget/Icon_Apps.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IconApps extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: 100,
+          width: 100,
+        )
+      ],
+    );
+  }
+}

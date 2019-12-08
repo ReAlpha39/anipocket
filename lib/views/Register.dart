d260fa0755308612ed0b18508edb030fe10948df
ramadhanfn <ramadhanfirda27@gmail.com>
2019-12-08 16:47:54 +0700

    Set IconApps and set position Icon

--- lib/views/Register.dart
+++ lib/views/Register.dart
@@ -1,10 +1,24 @@
+import 'package:anipocket/widget/Icon_Apps.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        color: Colors.transparent,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 60),
+                child: IconApps(),
+              ),
+        ),
+      ),
     );
   }
 }

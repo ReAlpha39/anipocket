e25f5975522d9106747dad5b0a6a36790e410b8e
Erwin Santosa <erwincoc232@gmail.com>
2019-12-08 15:40:03 +0700

    Import package YoutubePlayerController & JikanApi

--- lib/widget/video_trailer.dart
+++ lib/widget/video_trailer.dart
@@ -1,4 +1,6 @@
+import 'package:anipocket/repositories/jikan_api.dart';
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoTrailer extends StatefulWidget {
   @override
@@ -6,6 +8,9 @@ class VideoTrailer extends StatefulWidget {
 }
 
 class _VideoTrailerState extends State<VideoTrailer> {
+  YoutubePlayerController _controller;
+  String vId;
+  var api = JikanApi();
   @override
   Widget build(BuildContext context) {
     return Container(

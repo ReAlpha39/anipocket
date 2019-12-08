9f6120c4e3618c35212d635d69951f62bdeb115f
Riyantama <riyantama2016@gmail.com>
2019-12-08 23:05:12 +0700

    add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:anipocket/views/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:anipocket/widget/bloc.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return StreamBuilder(
+      stream: bloc.darkThemeEnabled,
+      initialData: false,
+      builder: (context, snapshot) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: snapshot.data ? ThemeData.dark() : ThemeData.light(),
+        home: Scaffold(
+          appBar: AppBar(title: Text('AniPocket'), actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            )
+          ]),
+        ),
       ),
-      home: HomePage(),
     );
   }
 }

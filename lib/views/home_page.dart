6c1f0e02a9e4b538549b7301832ced738c23139c
YunusNS <yunusw718@gmail.com>
2019-12-08 11:54:55 +0700

    clean up ui

---
+++ lib/views/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('AniPocket', style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: Container(),
+    );
+  }
+}

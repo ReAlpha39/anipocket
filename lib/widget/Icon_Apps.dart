6a5de57eb12115c1d41f2b74510ad8db69decf1d
ramadhanfn <ramadhanfirda27@gmail.com>
2019-12-08 15:47:14 +0700

    set positon icon

--- lib/widget/Icon_Apps.dart
+++ lib/widget/Icon_Apps.dart
@@ -4,8 +4,10 @@ class IconApps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: Alignment.center,
       children: <Widget>[
         Container(
+          margin: EdgeInsets.only(left: 0, top: 0),
           height: 100,
           width: 100,
         )

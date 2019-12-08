a2d1a673d868ce2167b80ca4cfa7cbfc28aefb6f
Riyantama <riyantama2016@gmail.com>
2019-12-08 14:11:04 +0700

    add title list & image anime

--- lib/views/top_anime_view.dart
+++ lib/views/top_anime_view.dart
@@ -1,3 +1,5 @@
+import 'package:anipocket/models/models.dart';
+import 'package:anipocket/widget/top_anime_list.dart';
 import 'package:flutter/material.dart';
 
 class TopAnimeView extends StatelessWidget {
@@ -9,7 +11,37 @@ class TopAnimeView extends StatelessWidget {
           icon: Icon(Icons.search),
           onPressed: () {},
         )
-      ]),
+      ]
+      ),
+      body:  SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    "Top  Anime",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 700,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ListView(
+                children: <Widget>[
+                  TopAnimeList()
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/widget/top_anime_list.dart
+++ lib/widget/top_anime_list.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
 
+class TopAnimeList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Card(
+          elevation: 5,
+          child: Container(
+            padding: EdgeInsets.all(8),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  height: 200,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage('https://cdn.myanimelist.net/images/anime/5/87048l.jpg')
+                          )
+                          ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}

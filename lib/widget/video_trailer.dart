36071fba3c98a7d5d45923d1b17a447e763ad0ca
Erwin Santosa <erwincoc232@gmail.com>
2019-12-08 15:36:32 +0700

    Add widget video trailer

---
+++ lib/widget/video_trailer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoTrailer extends StatefulWidget {
+  @override
+  _VideoTrailerState createState() => _VideoTrailerState();
+}
+
+class _VideoTrailerState extends State<VideoTrailer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}

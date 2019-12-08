34a1b9af724b3d52b398c0f99303b40b9d983be3
Riyantama <riyantama2016@gmail.com>
2019-12-08 17:08:30 +0700

    add keterangan manga

--- lib/widget/top_manga_list.dart
+++ lib/widget/top_manga_list.dart
@@ -24,6 +24,63 @@ class TopMangaList extends StatelessWidget {
                       image: NetworkImage(
                           'https://cdn.myanimelist.net/images/manga/1/157931l.jpg'))),
             ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  height: 200,
+                  width: 200,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Berserk',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Text(
+                            'Score : 9.30',
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ),
+                         Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Text(
+                            'Rank : #1',
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Text(
+                            'Manga',
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Text(
+                            'Aug 25, 1989 to ?',
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Text(
+                            'Genre :  Action, Adventure, Demons, Drama, Fantasy, Horror, Supernatural, Military, Psychological, Seinen',
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
           ]),
         ),
       )

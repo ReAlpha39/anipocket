7c59756ae73d51dabe6f0cad1a223550e848edc0
ramadhanfn <ramadhanfirda27@gmail.com>
2019-12-08 16:54:24 +0700

    Add container for button register

--- lib/views/Register.dart
+++ lib/views/Register.dart
@@ -5,65 +5,75 @@ class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        color: Colors.transparent,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 60),
-                child: IconApps(),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0, bottom: 20.0),
-                child: Text(
-                  "AniPocket",
-                  style: TextStyle(fontSize: 30.0),
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          color: Colors.transparent,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 60),
+                  child: IconApps(),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 20, left: 40, right: 40, bottom: 0),
-                child: TextField(
-                  decoration: InputDecoration(
-                      labelText: 'User Name',
-                      prefixIcon: Icon(
-                        Icons.account_circle,
-                        size: 18,
-                      )
-                      ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 20.0),
+                  child: Text(
+                    "AniPocket",
+                    style: TextStyle(fontSize: 30.0),
+                  ),
                 ),
-              ),
-               Padding(
-                padding: const EdgeInsets.only(
-                    top: 20, left: 40, right: 40, bottom: 0),
-                child: TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Email',
-                      prefixIcon: Icon(
-                        Icons.email,
-                        size: 18,
-                      )),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 20, left: 40, right: 40, bottom: 0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: 'User Name',
+                        prefixIcon: Icon(
+                          Icons.account_circle,
+                          size: 18,
+                        )),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 20, left: 40, right: 40, bottom: 0),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: 'Password',
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        size: 18,
-                      )),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 20, left: 40, right: 40, bottom: 0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: 'Email',
+                        prefixIcon: Icon(
+                          Icons.email,
+                          size: 18,
+                        )),
+                  ),
                 ),
-              ),
-            ]),
-      ),
-    );
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 20, left: 40, right: 40, bottom: 0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: 'Password',
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          size: 18,
+                        )),
+                  ),
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                          child: Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 40.0, right: 40.0, top: 40.0),
+                              child: Container(
+                                alignment: Alignment.center,
+                                height: 50.0,
+                              )))
+                    ])
+              ]),
+        ));
   }
 }

30e06042aa61262da19afa01a6ebc584010f795e
ramadhanfn <ramadhanfirda27@gmail.com>
2019-12-08 15:55:47 +0700

    create container for icon

--- lib/views/Login_Page.dart
+++ lib/views/Login_Page.dart
@@ -5,7 +5,20 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Container(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        color: Colors.transparent,
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 60),
+                
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

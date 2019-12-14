b74c0f5b87f23330d494795027dd2d8b7dddd455
Yunus Niandy S <yunusw718@gmail.com>
2019-12-14 22:38:13 +0700

    add thunk action untuk membuat http request secara async

--- lib/redux/action_season_list.dart
+++ lib/redux/action_season_list.dart
@@ -1,4 +1,8 @@
 import 'package:anipocket/models/season/season_anime.dart';
+import 'package:anipocket/models/season/season_type.dart';
+import 'package:anipocket/redux/appstate_season_list.dart';
+import 'package:anipocket/repositories/jikan_api.dart';
+import 'package:redux_thunk/redux_thunk.dart';
 
 class GotData {
   SeasonAnime results;
@@ -8,4 +12,57 @@ class GotData {
   SeasonAnime get seasonAnime => this.results;
 
   GotData({this.results, this.season, this.year});
+}
+
+ThunkAction<AppStateSeasonList> getSeasonAnime({String season}) {
+  return (store) async {
+    JikanApi jikanApi = JikanApi();
+    switch (season) {
+      case 'Winter':
+        {
+          store.dispatch(GotData(
+            results: await jikanApi.getSeasonAnime(2019, SeasonType.WINTER),
+            season: season,
+            year: '2019'
+          ));
+        }
+        break;
+
+      case 'Spring':
+        {
+          store.dispatch(GotData(
+            results: await jikanApi.getSeasonAnime(2019, SeasonType.SPRING),
+            season: season,
+            year: '2019'
+          ));
+        }
+        break;
+
+      case 'Summer':
+        {
+          store.dispatch(GotData(
+            results: await jikanApi.getSeasonAnime(2019, SeasonType.SUMMER),
+            season: season,
+            year: '2019'
+          ));
+        }
+        break;
+
+      case 'Fall':
+        {
+          store.dispatch(GotData(
+            results: await jikanApi.getSeasonAnime(2019, SeasonType.FALL),
+            season: season,
+            year: '2019'
+          ));
+        }
+        break;
+      default:
+        {
+          store.dispatch(GotData(
+            results: await jikanApi.getSeasonAnime(2019, SeasonType.WINTER)
+          ));
+        }
+    }
+  };
 }

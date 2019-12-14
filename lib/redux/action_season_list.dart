f0521bb359d15686d797f2e75bdfaea345c41b22
Yunus Niandy S <yunusw718@gmail.com>
2019-12-14 22:45:37 +0700

    list season menampilkan 2 season kedepan 1 season sebelumnya berdasarkan season sekarang

--- lib/redux/action_season_list.dart
+++ lib/redux/action_season_list.dart
@@ -2,6 +2,7 @@ import 'package:anipocket/models/season/season_anime.dart';
 import 'package:anipocket/models/season/season_type.dart';
 import 'package:anipocket/redux/appstate_season_list.dart';
 import 'package:anipocket/repositories/jikan_api.dart';
+import 'package:intl/intl.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 
 class GotData {
@@ -15,52 +16,99 @@ class GotData {
 }
 
 ThunkAction<AppStateSeasonList> getSeasonAnime({String season}) {
+  DateTime _now = DateTime.now();
+  String _thisMonth = DateFormat("MM").format(_now);
+  int _thisYear = int.parse(DateFormat("yyyy").format(_now));
+  int _year;
+  List<String> winter = ['1','2','3'];
+  List<String> spring = ['4','5','6'];
+  List<String> summer = ['7','8','9'];
+  List<String> fall = ['10','11','12'];
+  SeasonType _seasonType = SeasonType.WINTER;
+  String _thisSeason = '';
+  if (winter.contains(_thisMonth)) {
+    _seasonType = SeasonType.WINTER;
+    _thisSeason = 'winter';
+  } else if (spring.contains(_thisMonth)) {
+    _seasonType = SeasonType.SPRING;
+    _thisSeason = 'spring';
+  } else if (summer.contains(_thisMonth)) {
+    _seasonType = SeasonType.SUMMER;
+    _thisSeason = 'summer';
+  } else if (fall.contains(_thisMonth)) {
+    _seasonType = SeasonType.FALL;
+    _thisSeason = 'fall';
+  }
   return (store) async {
     JikanApi jikanApi = JikanApi();
     switch (season) {
       case 'Winter':
         {
+          if (_thisSeason == 'fall' || _thisSeason == 'summer') {
+            int _nextYear = _thisYear + 1;
+            _year = _nextYear;
+          } else {
+            _year = _thisYear;
+          }
           store.dispatch(GotData(
-            results: await jikanApi.getSeasonAnime(2019, SeasonType.WINTER),
+            results: await jikanApi.getSeasonAnime(_year, SeasonType.WINTER),
             season: season,
-            year: '2019'
+            year: _year.toString()
           ));
         }
         break;
 
       case 'Spring':
         {
+          if (_thisSeason == 'winter' || _thisSeason == 'fall') {
+            int _nextYear = _thisYear + 1;
+            _year = _nextYear;
+          } else {
+            _year = _thisYear;
+          }
           store.dispatch(GotData(
-            results: await jikanApi.getSeasonAnime(2019, SeasonType.SPRING),
+            results: await jikanApi.getSeasonAnime(_year, SeasonType.SPRING),
             season: season,
-            year: '2019'
+            year: _year.toString()
           ));
         }
         break;
 
       case 'Summer':
         {
+          if (_thisSeason == 'spring' || _thisSeason == 'winter') {
+            int _nextYear = _thisYear + 1;
+            _year = _nextYear;
+          } else {
+            _year = _thisYear;
+          }
           store.dispatch(GotData(
-            results: await jikanApi.getSeasonAnime(2019, SeasonType.SUMMER),
+            results: await jikanApi.getSeasonAnime(_year, SeasonType.SUMMER),
             season: season,
-            year: '2019'
+            year: _year.toString()
           ));
         }
         break;
 
       case 'Fall':
         {
+          if (_thisSeason == 'summer' || _thisSeason == 'spring') {
+            int _nextYear = _thisYear + 1;
+            _year = _nextYear;
+          } else {
+            _year = _thisYear;
+          }
           store.dispatch(GotData(
-            results: await jikanApi.getSeasonAnime(2019, SeasonType.FALL),
+            results: await jikanApi.getSeasonAnime(_year, SeasonType.FALL),
             season: season,
-            year: '2019'
+            year: _year.toString()
           ));
         }
         break;
       default:
         {
           store.dispatch(GotData(
-            results: await jikanApi.getSeasonAnime(2019, SeasonType.WINTER)
+            results: await jikanApi.getSeasonAnime(_thisYear, _seasonType)
           ));
         }
     }

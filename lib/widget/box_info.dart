bc6a674e4f1c938ad23ca396347d151fa4d7e3cd
Erwin Santosa <erwincoc232@gmail.com>
2019-12-15 21:08:09 +0700

    Add text and icon for Watch List

--- lib/widget/box_info.dart
+++ lib/widget/box_info.dart
@@ -17,36 +17,67 @@ class _BoxInfoState extends State<BoxInfo> {
           child: Row(
             children: <Widget>[
               Column(
-                  children: <Widget>[
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.favorite,
-                          color: Colors.white,
-                          size: 30,
-                        ),
-                        SizedBox(
-                          width: 4,
-                        ),
-                        Text(
-                          "99",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 24),
-                        )
-                      ],
-                    ),
-                    Text(
-                      "Favorite",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 15),
-                    )
-                  ],
-                ),
+                children: <Widget>[
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        "99",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 24),
+                      )
+                    ],
+                  ),
+                  Text(
+                    "Favorite",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15),
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        "99",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 24),
+                      )
+                    ],
+                  ),
+                  Text(
+                    "Watch List",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15),
+                  )
+                ],
+              ),
             ],
           ),
         ),

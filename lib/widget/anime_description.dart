933468b8e154e1a5a0d0fe397969264726ab1e3d
Riyantama <riyantama2016@gmail.com>
2019-12-15 22:06:17 +0700

    add anime image

---
+++ lib/widget/anime_description.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AnimeDescription extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(5),
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 250,
+                width: 150,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(5),
+                      topLeft: Radius.circular(5),
+                      topRight: Radius.circular(5),
+                      bottomRight: Radius.circular(5),
+                    ),
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://cdn.myanimelist.net/images/anime/1630/103417.jpg'))),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}

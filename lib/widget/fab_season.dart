74abc0f5f70d607eb4bdb6a2fc5cec968658c3bd
Yunus Niandy S <yunusw718@gmail.com>
2019-12-15 16:11:38 +0700

    change action pada setiap button untuk menapilkan data list anime sesuai season yang dipilih

--- lib/widget/fab_season.dart
+++ lib/widget/fab_season.dart
@@ -1,48 +1,61 @@
+import 'package:anipocket/redux/action_season_list.dart';
+import 'package:anipocket/redux/appstate_season_list.dart';
+import 'package:anipocket/redux/store_season_list.dart';
+import 'package:anipocket/redux/view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class FabSeason extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SpeedDial(
+    return StoreConnector<AppStateSeasonList, ViewModel>(
+      converter: (store) {
+        return ViewModel(title: store.state.title);
+      }, builder: (context, viewModel) {
+        return SpeedDial(
           animatedIcon: AnimatedIcons.menu_close,
           animatedIconTheme: IconThemeData(size: 22.0),
-          closeManually: false,
           curve: Curves.bounceIn,
           overlayOpacity: 0,
-          elevation: 8.0,
           shape: CircleBorder(),
           children: [
             SpeedDialChild(
+              elevation: 6,
               child: Icon(FontAwesomeIcons.leaf, size: 16,),
               backgroundColor: Colors.red,
               label: 'Fall',
-              labelStyle: TextStyle(fontSize: 18.0),
-              onTap: () => print('FIRST CHILD')
+              labelStyle: TextStyle(fontSize: 16.0),
+              onTap: () => store.dispatch(getSeasonAnime(season: 'Fall'))
             ),
             SpeedDialChild(
+              elevation: 6,
               child: Icon(Icons.wb_sunny),
               backgroundColor: Colors.blue,
               label: 'Summer',
-              labelStyle: TextStyle(fontSize: 18.0),
-              onTap: () => print('SECOND CHILD'),
+              labelStyle: TextStyle(fontSize: 16.0),
+              onTap: () => store.dispatch(getSeasonAnime(season: 'Summer'))
             ),
             SpeedDialChild(
+              elevation: 6,
               child: Icon(Icons.local_florist),
               backgroundColor: Colors.green,
               label: 'Spring',
-              labelStyle: TextStyle(fontSize: 18.0),
-              onTap: () => print('THIRD CHILD'),
+              labelStyle: TextStyle(fontSize: 16),
+              onTap: () => store.dispatch(getSeasonAnime(season: 'Spring'))
             ),
             SpeedDialChild(
+              elevation: 6,
               child: Icon(FontAwesomeIcons.snowflake, size: 20,),
               backgroundColor: Colors.green,
-              label: 'WInter',
-              labelStyle: TextStyle(fontSize: 18.0),
-              onTap: () => print('FOURTH CHILD'),
+              label: 'Winter',
+              labelStyle: TextStyle(fontSize: 16.0),
+              onTap: () => store.dispatch(getSeasonAnime(season: 'Winter'))
             ),
           ],
         );
+      }
+    );
   }
 }

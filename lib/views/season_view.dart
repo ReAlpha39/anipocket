89c6a6c005622b251faf08b9bcb1c96a6baf32c6
Yunus Niandy S <yunusw718@gmail.com>
2019-12-15 15:29:48 +0700

    menampilkan list sesuai dengan action dari actionbutton

--- lib/views/season_view.dart
+++ lib/views/season_view.dart
@@ -1,6 +1,8 @@
+import 'package:anipocket/redux/action_season_list.dart';
 import 'package:anipocket/redux/appstate_season_list.dart';
 import 'package:anipocket/redux/store_season_list.dart';
 import 'package:anipocket/redux/view_model.dart';
+import 'package:anipocket/widget/CardAnime.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
@@ -22,12 +24,28 @@ class SeasonView extends StatelessWidget {
           return ViewModel(results: store.state.seasonAnime);
         },
         builder: (context, viewModel) {
-          return Container();
+          print(store.state.seasonAnime);
+          return Container(
+            child: store.state.seasonAnime == null
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : ListView.builder(
+                  itemCount: viewModel.results.anime.length,
+                  itemBuilder: (context, index) {
+                    if (viewModel.results.anime[index].r18 == false) {
+                      return CardAnime(anime: viewModel.results.anime[index]);
+                    } else {
+                      return Container();
+                    }
+                  },
+                ),
+          );
         },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.ac_unit),
-        onPressed: (){},
+        onPressed: () {store.dispatch(getSeasonAnime(season: 'Spring'));},
       ),
     );
   }

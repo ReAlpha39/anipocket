fb1ed00f548c63875571266dd08676bc0d2ae58c
Erwin Santosa <erwincoc232@gmail.com>
2019-12-15 16:54:27 +0700

    Add AppBar user profile

--- lib/views/user_profile.dart
+++ lib/views/user_profile.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        elevation: 0.0,
+        iconTheme: new IconThemeData(color: Color(0xFF18D191)),
+        title: Text('User Profile'),
+      ),
     );
   }
 }

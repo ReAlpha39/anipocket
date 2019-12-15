97fe129a430aea8dacb322768f259e095f137fd5
Erwin Santosa <erwincoc232@gmail.com>
2019-12-15 18:37:21 +0700

    Add widget BoxInfo untuk menampilkan box favorit and watchlist

---
+++ lib/widget/box_info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BoxInfo extends StatefulWidget {
+  @override
+  _BoxInfoState createState() => _BoxInfoState();
+}
+
+class _BoxInfoState extends State<BoxInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}

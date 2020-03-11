84ba8a4dde3c34e97914d020321b4ca0550ca4b5
Yunus Niandy S <yunusw718@gmail.com>
2020-03-11 21:40:14 +0700

    add ThemeState to hold ThemeData object to be used by MaterialApp

---
+++ lib/bloc/theme_state.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+class ThemeState extends Equatable {
+  final ThemeData themeData;
+
+  ThemeState({@required this.themeData});
+
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [themeData];
+}

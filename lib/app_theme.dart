4917ddb8fde88f1c29138433a81c05e696e6d150
Yunus Niandy S <yunusw718@gmail.com>
2020-03-11 23:50:15 +0700

    edit some color on custom theme, & remove color from some widget

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -9,9 +9,15 @@ final appThemeData = {
   AppTheme.LightTheme: ThemeData(
     brightness: Brightness.light,
     primaryColor: Color.fromARGB(255, 7, 48, 66),
+    appBarTheme: AppBarTheme(color: Color.fromARGB(255, 7, 48, 66)),
   ),
   AppTheme.DarkTheme: ThemeData(
     brightness: Brightness.dark,
     primaryColor: Color.fromARGB(255, 7, 48, 66),
+    primaryColorDark: Colors.grey[900],
+    accentColor: Colors.indigo[700],
+    appBarTheme: AppBarTheme(color: Color.fromARGB(255, 7, 48, 66)),
+    canvasColor: Colors.grey[900]
+
   )
 };
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -11,7 +11,6 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('AniPocket', style: GoogleFonts.raleway(textStyle: TextStyle(color: Colors.white))),
         centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 7, 48, 66)
       ),
       drawer: Menu(),
       body: SingleChildScrollView(
